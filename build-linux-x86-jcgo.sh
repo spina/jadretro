34577f0794a1d4557728daf440a47684767d663d
Ivan Maidanski <ivmai@mail.ru>
2009-12-17 13:03:55 +0400

    2009-12-17  Ivan Maidanski <ivmai@mail.ru>
    * build.xml (dist.project.tar.file): Remove unused property.
    * build-linux-x86-jcgo.sh: Explicitly set "rwxr-xr-x" mode for the
    project directory and the executable file.
    * build-linux-x86-jcgo.sh: Set SVN executable file flag.

--- build-linux-x86-jcgo.sh
+++ build-linux-x86-jcgo.sh
@@ -15,10 +15,11 @@ mkdir "$dist_dir/.jcgo_Out"
 
 $jcgo_home/jcgo -d "$dist_dir/.jcgo_Out" -src $~/goclsp/clsp_asc -src src net.sf.$proj_unix_name.Main @$~/stdpaths.in || exit 1
 
-mkdir "$dist_dir/$proj_unix_name"
+mkdir -m 0755 "$dist_dir/$proj_unix_name"
 gcc -o "$dist_dir/$proj_unix_name/$proj_unix_name" -I $jcgo_home/include -I $jcgo_home/include/boehmgc -I $jcgo_home/native -Os -fwrapv -fno-strict-aliasing -freorder-blocks -DJCGO_INTFIT -DJCGO_UNIX -DJCGO_UNIFSYS -DJCGO_USEGCJ -DJCGO_NOJNI -DJCGO_NOSEGV -DEXTRASTATIC=static -DJNIIMPORT=static/**/inline -DJNIEXPORT=JNIIMPORT -DJNUBIGEXPORT=static -DGCSTATICDATA= -DJCGO_GCRESETDLS -DJCGO_NOFP -s "$dist_dir/.jcgo_Out/Main.c" $jcgo_home/libs/x86/linux/libgc.a || exit 1
 
 cp -p GNU_GPL.txt $proj_unix_name.txt "$dist_dir/$proj_unix_name"
+chmod 0755 "$dist_dir/$proj_unix_name/$proj_unix_name"
 echo ""
 
 "$dist_dir/$proj_unix_name/$proj_unix_name"
